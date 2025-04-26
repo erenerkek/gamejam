3d052417d05ceca380eee1321c9e65f0853f73d3
Eren Erkek <erkekeren14@gmail.com>
2025-04-27 00:18:37 +0300

    kamera fokus

---
+++ geymjam/Assets/Scripts/FloatScriptable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewFloat", menuName = "ScriptableObjects/FloatScriptable")]
+public class FloatScriptable : ScriptableObject
+{
+    public float value;
+}
