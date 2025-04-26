a1f9ea9a91b7396af76e0640b4ff9f5cef084de9
yakuğ <hasanyakuparslan@gmail.com>
2025-04-26 06:53:01 +0300

    karakter hareketleri

---
+++ geymjam/Assets/Player_Movement.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+    public Animator animator;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private Transform firePoint;
+
+
+
+    bool isGrounded = false;
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        animator.SetBool("IsGrounded", isGrounded);
+
+        // Jump input
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (!isGrounded)
+        {
+            if (rb.velocity.y > 0.1f)
+            {
+                // Yukarý doðru giderken Jump animasyonu
+                animator.SetBool("IsJumping", true);
+            }
+            else if (rb.velocity.y <= 0)
+            {
+                // Aþaðý düþmeye baþladýysa Fall tetikle
+                animator.SetBool("IsJumping", false);
+                animator.SetTrigger("IsFalling"); // Trigger'ý burada her zaman set et
+            }
+        }
+        else if (isGrounded && rb.velocity.y <= 0f)
+        {
+            // Yere indiðinde her þeyi sýfýrla
+            animator.SetBool("IsJumping", false);
+            animator.ResetTrigger("IsFalling");
+        }
+
+        Flip();
+    }
+
+
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+
+            if(localScale.x < 0f)
+            {
+                firePoint.localRotation = new Quaternion(0f, 0f, 0f, firePoint.rotation.w);
+            }
+            else
+            {
+                firePoint.localRotation = new Quaternion(0f, 180f, 0f, firePoint.rotation.w);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, 0.2f);
+        }
+    }
+}
