cef4a58990cbf44ec8d97a9ea9febc9023aeda97
Eren Erkek <erkekeren14@gmail.com>
2025-04-27 12:43:56 +0300

    aaa

--- geymjam/Assets/Scripts/EnemyHealth.cs
+++ geymjam/Assets/Scripts/EnemyHealth.cs
@@ -1,24 +1,46 @@
 using UnityEngine;
-
+using System.Collections;
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            originalColor = spriteRenderer.color;
+        }
+
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
+         if (spriteRenderer != null)
+        {
+            StartCoroutine(DamageFlash());
+        }
+
         if (currentHealth <= 0)
         {
             Die();
         }
     }
+
+     private IEnumerator DamageFlash()
+    {
+        spriteRenderer.color = Color.red; // Hasar alınca kırmızı yap
+        yield return new WaitForSeconds(0.2f); // (İstersen enemy için 0.2 saniye yapalım, hızlıca parlasın)
+        spriteRenderer.color = originalColor; // Eski renge geri dön
+    }
+
 
     public virtual void Die()
     {
