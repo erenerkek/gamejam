fb61d60d0d299f3f0a8c8adc9c908122827cf02d
Eren Erkek <erkekeren14@gmail.com>
2025-04-27 06:14:50 +0300

    kapı falan

---
+++ geymjam/Assets/Scripts/DoorTrigger1.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;  // SceneManager'ı kullanabilmek için gerekli
+
+public class DoorTrigger : MonoBehaviour
+{
+    public string sceneToLoad;  // Geçilecek scene adı (Inspector üzerinden ayarlanacak)
+    private bool isPlayerNear = false;  // Oyuncu kapının yakınında mı kontrolü
+
+    void Update()
+    {
+        // Eğer oyuncu kapıya yakınsa ve E tuşuna basılmışsa
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            LoadLevel();  // Yeni scene'e geçiş yap
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Eğer oyuncu kapıya yaklaşırsa, 'E' tuşu için hazır duruma geliriz
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // Eğer oyuncu kapıdan uzaklaşırsa, 'E' tuşunu artık kabul etmeyiz
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+        }
+    }
+
+    void LoadLevel()
+    {
+        // Belirtilen scene'i yükler
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
